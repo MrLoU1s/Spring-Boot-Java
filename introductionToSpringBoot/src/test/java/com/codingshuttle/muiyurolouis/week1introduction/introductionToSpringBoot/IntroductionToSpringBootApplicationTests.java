5ed2def9f44b8d5553af7ace0db5bc27bdc0e5eb
MrLoU1s <louis.karanja@strathmore.edu>
2024-08-20 21:02:05 +0300

    1.2 setting up

---
+++ introductionToSpringBoot/src/test/java/com/codingshuttle/muiyurolouis/week1introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.muiyurolouis.week1introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
